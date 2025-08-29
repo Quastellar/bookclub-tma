d620c84558a3440c16327ffa59d744cacc11ebca
yolotex <blazarray@gmail.com>
2025-08-29 23:45:12 +0300

    Исправлена ошибка в разметке компонента AdminPage: удален лишний символ перед текстом "Текущая итерация".

--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -350,7 +350,7 @@ export default function AdminPage() {
                         color: '#1f2937',
                         margin: '0 0 16px 0'
                     }}>
-шв                         Текущая итерация
+                        Текущая итерация
                     </h2>
 
                     {currentIter ? (
